56b69c6bffe4b97e38f991797532355ef97cec2e
Georg Apitz <georg@usebar.org>
2013-09-20 12:44:26 -0600

    add keys and merge

--- intro/src/KVStore.js
+++ intro/src/KVStore.js
@@ -1,16 +1,33 @@
 function KVStore() {
-    var innerStore = {};
+  var innerStore = {};
 
-    this.set = function (key, value) {
-        innerStore[key] = value;
-    };
+  this.set = function (key, value) {
+    innerStore[key] = value;
+  };
 
-    this.get = function(key) {
-        return innerStore[key];
-    };
+  this.get = function (key) {
+    return innerStore[key];
+  };
 
-    this.merge = function(otherStore){
-       var finalStore = new KVStore();
-       finalStore.set("")
+  this.merge = function (otherStore) {
+    var finalStore = new KVStore();
+
+    this.keys().forEach(function (key) {
+      finalStore.set(key, this.get(key));
+    }, this);
+
+    otherStore.keys().forEach(function (key) {
+      finalStore.set(key, otherStore.get(key));
+    });
+
+    return finalStore;
+  };
+
+  this.keys = function () {
+    var keys = [];
+    for (var key in innerStore) {
+      keys.push(key);
     }
+    return keys;
+  };
 }
