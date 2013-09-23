53af799d03a806211f0a11310834bc4666a4b6e9
Georg Apitz and Zac Clark <pair+georg+zclark@pivotallabs.com>
2013-09-23 12:29:39 -0600

    Add findAll specs

--- v-todo/spec/storeSpec.js
+++ v-todo/spec/storeSpec.js
@@ -1,8 +1,9 @@
 describe("Store", function () {
-  var store;
+  var store, noop;
 
   beforeEach(function () {
     store = new app.Store("test store");
+    noop = function () {};
   });
 
   afterEach(function () {
@@ -16,6 +17,45 @@ describe("Store", function () {
       store.save(obj, callback);
 
       expect(callback).toHaveBeenCalled();
+    });
+  });
+
+  describe("findAll", function () {
+    it("calls back with an empty array when empty", function () {
+      var callbackSpy = jasmine.createSpy("callback");
+      store.findAll(callbackSpy);
+
+      expect(callbackSpy).toHaveBeenCalledWith([]);
+    });
+
+    it("calls back with each item that is added", function () {
+      store.save({a: "A"}, noop);
+      store.save({b: "B"}, noop);
+      store.save({c: "C"}, noop);
+
+      var callbackSpy = jasmine.createSpy("callback");
+      store.findAll(callbackSpy);
+
+//      expect(callbackSpy).toHaveBeenCalledWith([
+//        {id: 1, a: "A"},
+//        {id: 2, b: "B"},
+//        {id: 3, c: "C"}
+//      ]);
+
+      expect(callbackSpy).toHaveBeenCalled();
+      var callbackData = callbackSpy.mostRecentCall.args[0];
+      expect(callbackData.length).toEqual(3);
+      expect(callbackData).toContain({a: "A", id: jasmine.any(Number)});
+      expect(callbackData).toContain({b: "B", id: jasmine.any(Number)});
+      expect(callbackData).toContain({c: "C", id: jasmine.any(Number)});
+    });
+
+    it("does not crash when called with no arguments", function () {
+      try {
+        store.findAll();
+      } catch (e) {
+        throw "Store#findAll threw!";
+      }
     });
   });
 });
