1cdd5cebfe501fa20bb6e61dd2b3718d73db1765
Georg Apitz and Zac Clark <pair+georg+zclark@pivotallabs.com>
2013-09-23 12:56:21 -0600

    Add specs for find and constructor

--- v-todo/spec/storeSpec.js
+++ v-todo/spec/storeSpec.js
@@ -1,28 +1,54 @@
 describe("Store", function () {
-  var store, noop;
+  var store, noop, callbackSpy;
 
   beforeEach(function () {
     store = new app.Store("test store");
-    noop = function () {};
+    noop = function () { };
+    callbackSpy = jasmine.createSpy("callback");
   });
 
   afterEach(function () {
-    store.drop(function () {});
+    store.drop(noop);
+  });
+
+  describe("constructor", function(){
+    it("calls the callback with the local store name", function () {
+      var testStore = new app.Store("test store", callbackSpy);
+
+      expect(callbackSpy).toHaveBeenCalledWith({ todos : [ ] });
+    });
+
+    it("does not crash when called with no callback", function () {
+      expect(function () {
+        new app.Store("test store");
+      }).not.toThrow();
+    });
+
+    it("shares data betweeen stores using the same name", function () {
+      var storeOne = new app.Store("myStore");
+      storeOne.drop(noop);
+      var storeTwo = new app.Store("myStore");
+      storeTwo.drop(noop);
+
+      storeOne.save({an: "item"}, noop);
+      storeTwo.findAll(callbackSpy);
+
+      expect(callbackSpy).toHaveBeenCalledWith([{an: "item", id: jasmine.any(Number)}]);
+    });
+
   });
 
   describe("save", function () {
     it("calls back when done", function () {
       var obj = {foo: "bar"};
-      var callback = jasmine.createSpy("callback");
-      store.save(obj, callback);
+      store.save(obj, callbackSpy);
 
-      expect(callback).toHaveBeenCalled();
+      expect(callbackSpy).toHaveBeenCalled();
     });
   });
 
   describe("findAll", function () {
     it("calls back with an empty array when empty", function () {
-      var callbackSpy = jasmine.createSpy("callback");
       store.findAll(callbackSpy);
 
       expect(callbackSpy).toHaveBeenCalledWith([]);
@@ -33,7 +59,6 @@ describe("Store", function () {
       store.save({b: "B"}, noop);
       store.save({c: "C"}, noop);
 
-      var callbackSpy = jasmine.createSpy("callback");
       store.findAll(callbackSpy);
 
 //      expect(callbackSpy).toHaveBeenCalledWith([
@@ -51,11 +76,37 @@ describe("Store", function () {
     });
 
     it("does not crash when called with no arguments", function () {
-      try {
+      expect(function () {
         store.findAll();
-      } catch (e) {
-        throw "Store#findAll threw!";
-      }
+      }).not.toThrow();
+    });
+  });
+
+  describe("find", function () {
+    it("calls back with an empty array when the store is empty", function () {
+      store.find({}, callbackSpy);
+
+      expect(callbackSpy).toHaveBeenCalledWith([]);
+    });
+
+    it("returns matching objects from the store", function () {
+      store.save({foo: "bar", hello: "world"}, noop);
+      store.save({foo: "bar", bye: "planet"}, noop);
+      store.save({foo: "NOPE", stuff: "is cool"}, noop);
+
+      store.find({foo: "bar"}, callbackSpy);
+
+      expect(callbackSpy).toHaveBeenCalled();
+      var callbackData = callbackSpy.mostRecentCall.args[0];
+      expect(callbackData.length).toEqual(2);
+      expect(callbackData).toContain({foo: "bar", hello: "world", id: jasmine.any(Number)});
+      expect(callbackData).toContain({foo: "bar", bye: "planet", id: jasmine.any(Number)});
+    });
+
+    it("does not crash when called with no arguments", function () {
+      expect(function () {
+        store.find();
+      }).not.toThrow();
     });
   });
 });
