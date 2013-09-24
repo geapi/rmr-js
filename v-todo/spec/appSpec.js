89b7eaf66102255fbb2b815e876ea163f57e2eb7
Georg Apitz and Zac Clark <pair+georg+zclark@pivotallabs.com>
2013-09-24 17:55:18 -0600

    add acceptance specs

--- v-todo/spec/appSpec.js
+++ v-todo/spec/appSpec.js
@@ -14,4 +14,45 @@ describe("app", function () {
   it("has a store", function () {
     expect(app.Store).toBeDefined();
   });
+
+  describe("acceptance testing", function () {
+    var createItem;
+
+    beforeEach(function () {
+      createItem = function (name) {
+        document.getElementById("new-todo").value = name;
+        var e = new Event("keypress");
+        e.keyCode = 13;
+        document.getElementById("new-todo").dispatchEvent(e);
+      };
+    });
+
+    afterEach(function () {
+      var store = new app.Store('todos-vanillajs');
+      store.drop(function () {});
+    });
+
+    it("enables the adding of todo items", function () {
+      createItem("Make an acceptance test");
+
+      expect(document.getElementById("todo-count").innerText).toEqual("1 item left");
+    });
+
+    it("allows deletion of todo items", function () {
+      createItem("delete me");
+      createItem("keep me");
+
+      var lis = document.getElementsByTagName("li");
+      var todoList;
+      for (var i in lis) {
+        if (lis[i].innerText == "delete me") {
+          todoList = lis[i];
+        }
+      }
+
+      todoList.dispatchEvent({type: "click", target: todoList});
+
+      expect(document.getElementById("todo-count").innerText).toEqual("1 item left");
+    });
+  });
 });
